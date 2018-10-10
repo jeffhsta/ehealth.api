6dfb5017eaef78e6f546baf6dc92e88497061105
Myroslav Kiurchev <kiurchv@gmail.com>
2018-10-10 15:57:42 +0300

    remove duplicated phone and address object types

--- apps/graphql/lib/graphql_web/schema/person_types.ex
+++ apps/graphql/lib/graphql_web/schema/person_types.ex
@@ -124,38 +124,4 @@ defmodule GraphQLWeb.Schema.PersonTypes do
     value(:offline, as: "OFFLINE")
     value(:na, as: "NA")
   end
-
-  # TODO: move this object into own file
-  object :address do
-    field(:type, non_null(:address_type))
-    field(:country, non_null(:string))
-    field(:area, non_null(:string))
-    field(:region, :string)
-    field(:settlement, non_null(:string))
-    field(:settlement_type, non_null(:string))
-    field(:settlement_id, non_null(:id))
-    field(:street_type, :string)
-    field(:street, :string)
-    field(:building, non_null(:string))
-    field(:apartment, :string)
-    field(:zip, non_null(:string))
-  end
-
-  # TODO: move this enum into own file
-  enum :address_type do
-    value(:residence, as: "RESIDENCE")
-    value(:registration, as: "REGISTRATION")
-  end
-
-  # TODO: move this object into own file
-  object :phone do
-    field(:type, non_null(:phone_type))
-    field(:number, non_null(:string))
-  end
-
-  # TODO: move this enum into own file
-  enum :phone_type do
-    value(:mobile, as: "MOBILE")
-    value(:land_line, as: "LAND_LINE")
-  end
 end
